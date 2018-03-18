46650e8bdfcda8a296b2d143fab25228f9d11ccd
0xd34df00d <0xd34df00d@gmail.com>
2018-03-18 17:17:37 -0400

    Initial commit.

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for cppfilt
+
+## Unreleased changes
---
+++ src/System/Demangle.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module System.Demangle(demangle) where
+
+import Foreign.C
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+
+foreign import ccall "__cxa_demangle"
+  cxa_demangle :: CString -> CString -> Ptr CSize -> Ptr CInt -> CString
+
+demangle :: String -> IO (Maybe String)
+demangle str = withCString str $ \str' -> do
+  let res = cxa_demangle str' nullPtr nullPtr nullPtr
+  if res == nullPtr
+    then pure Nothing
+    else do
+      res' <- peekCString res
+      free res
+      pure $ Just res'
