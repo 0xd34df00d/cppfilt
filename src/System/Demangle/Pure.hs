37f98cdb7a9282e5ed7ad1d48538aac362e71969
0xd34df00d <0xd34df00d@gmail.com>
2018-03-19 02:34:40 -0400

    Added demangle{BS,LBS} to the pure API.

--- src/System/Demangle/Pure.hs
+++ src/System/Demangle/Pure.hs
@@ -1,8 +1,16 @@
 module System.Demangle.Pure where
 
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import System.IO.Unsafe
 
 import qualified System.Demangle as D
 
 demangle :: String -> Maybe String
 demangle = unsafePerformIO . D.demangle
+
+demangleBS :: BS.ByteString -> Maybe BS.ByteString
+demangleBS = unsafePerformIO . D.demangleBS
+
+demangleLBS :: LBS.ByteString -> Maybe LBS.ByteString
+demangleLBS = unsafePerformIO . D.demangleLBS
