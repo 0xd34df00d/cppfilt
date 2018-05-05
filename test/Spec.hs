86d50bcde8ec065015fc43f79479171867bbbc76
0xd34df00d <0xd34df00d@gmail.com>
2018-05-05 18:38:37 -0400

    Fixed tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Test.Hspec
+
+import qualified Data.ByteString.Char8 as BS
 
 import qualified System.Demangle as D
 import qualified System.Demangle.Pure as DP
 
-ioDemangledIs :: String -> String -> IO ()
+import Data.CStringRepresentable
+
+ioDemangledIs :: (Eq a, Show a, CStringRepresentable a) => a -> a -> IO ()
 ioDemangledIs mangled ref = do
   res <- D.demangle mangled
   res `shouldBe` Just ref
 
-pureDemangledIs :: String -> String -> IO ()
+pureDemangledIs :: (Eq a, Show a, CStringRepresentable a) => a -> a -> IO ()
 pureDemangledIs mangled ref = DP.demangle mangled `shouldBe` Just ref
 
 main :: IO ()
 main = hspec $ do
-  describe "IO demangle" $ do
-    it "demangles some operators" $ do
-      "_ZrsR11QDataStreamR5QUuid" `ioDemangledIs` "operator>>(QDataStream&, QUuid&)"
-  describe "pure demangle" $ do
-    it "demangles some operators" $ do
-      "_ZrsR11QDataStreamR5QUuid" `pureDemangledIs` "operator>>(QDataStream&, QUuid&)"
+  describe "Strings" $ do
+    describe "IO demangle" $ do
+      it "demangles some operators" $ do
+        "_ZrsR11QDataStreamR5QUuid" `ioDemangledIs` ("operator>>(QDataStream&, QUuid&)" :: String)
+    describe "pure demangle" $ do
+      it "demangles some operators" $ do
+        "_ZrsR11QDataStreamR5QUuid" `pureDemangledIs` ("operator>>(QDataStream&, QUuid&)" :: String)
+  describe "ByteStrings" $ do
+    describe "IO demangle" $ do
+      it "demangles some operators" $ do
+        "_ZrsR11QDataStreamR5QUuid" `ioDemangledIs` ("operator>>(QDataStream&, QUuid&)" :: String)
+    describe "pure demangle" $ do
+      it "demangles some operators" $ do
+        "_ZrsR11QDataStreamR5QUuid" `pureDemangledIs` ("operator>>(QDataStream&, QUuid&)" :: String)
