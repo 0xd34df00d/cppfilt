3c062dc6bfd7f0d13bac75595465ceaedc7d9728
0xd34df00d <0xd34df00d@gmail.com>
2018-05-04 20:19:55 -0400

    Documented System.Demangle.

--- src/System/Demangle.hs
+++ src/System/Demangle.hs
@@ -1,5 +1,19 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
+{-|
+ Module      : System.Demangle
+ Description : Functions for demangling C++ names
+ Stability   : experimental
+ Portability : wherever gcc lives
+
+ This module provides bindings to the platform C++ ABI demangling routines
+ (where the platform is currently limited to gcc), conveniently working with
+ 'String' and both lazy and strict 'ByteString's.
+
+ This module provides the raw 'IO'-living functions. Thin wrappers pretending
+ to be pure live in the "System.Demangle.Pure" module.
+ -}
+
 module System.Demangle(
     demangle,
     demangleBS,
@@ -16,6 +30,7 @@ import Foreign.Ptr
 foreign import ccall "__cxa_demangle"
   cxa_demangle :: CString -> CString -> Ptr CSize -> Ptr CInt -> IO CString
 
+-- |Try to demangle a 'String' with a mangled C++ name.
 demangle :: String -> IO (Maybe String)
 demangle str = withCString str $ \str' -> do
   res <- cxa_demangle str' nullPtr nullPtr nullPtr
@@ -26,8 +41,10 @@ demangle str = withCString str $ \str' -> do
       free res
       pure $ Just res'
 
+-- |Try to demangle a strict 'BS.ByteString' with a mangled C++ name.
 demangleBS :: BS.ByteString -> IO (Maybe BS.ByteString)
 demangleBS = fmap (fmap BS.pack) . demangle . BS.unpack
 
+-- |Try to demangle a lazy 'LBS.ByteString' with a mangled C++ name.
 demangleLBS :: LBS.ByteString -> IO (Maybe LBS.ByteString)
 demangleLBS = fmap (fmap LBS.pack) . demangle . LBS.unpack
